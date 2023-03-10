48f5fe0043ae1689d801ee65bb9f9e6ac13ea202
kobakoba5884 <kobanminmin58@gmail.com>
2023-03-10 22:16:48 +0900

    revise .eslintrc.cjs

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,5 +1,5 @@
 // https://medium.com/ovrsea/setup-a-typescript-project-with-eslint-prettier-and-vs-code-4ce4fcb76509
 module.exports = {
- semi: false,
- singleQuote: true,
+  semi: false,
+  singleQuote: true,
 }
